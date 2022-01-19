f4eaf2957424e7c2de0982b1802204d6453281f9
CarlosDevBr <cahzerohenrique@gmail.com>
2022-01-19 09:01:53 -0300

    mongoose implementado

---
+++ src/db/mongoose.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
+    useNewUrlParser: true,
+    useCreateIndex: true
+})
+
+const User = mongoose.model('User', {
+    name: {
+        type: String
+    },
+    age: {
+        type: Number
+    }
+})
+
+// const me = new User({
+//     name: 'Carlos',
+//     age: 'Mike'
+// })
+
+// me.save().then(() => {
+//     console.log(me)
+// }).catch(() => {
+//     console.log('Error!', error)
+// })
+
+const Task = mongoose.model('Task', {
+    description: {
+        type: String
+    },
+    completed: {
+        type: Boolean
+    }
+})
+
+const note = new Task({
+    description: 'fazer um software',
+    completed: false
+})
+
+note.save().then(() => {
+    console.log(note)
+}).catch(() => {
+    console.log('Error!', error)
+})
