4b8c56d61910f45d80140c83c4beaebf2ddbd2c6
CarlosDevBr <cahzerohenrique@gmail.com>
2022-01-19 10:37:21 -0300

    adicionando o modulo validator

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
@@ -7,40 +8,72 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 
 const User = mongoose.model('User', {
     name: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value){
+            if (!validator.isEmail(value)){
+                throw new Error('Email is invalid')
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 7,
+        trim: true,
+        validate(value){
+            if(value.toLowerCase().includes('password')){
+                throw new Error('Password cannot contain "password"')
+            }
+        }
     },
     age: {
-        type: Number
+        type: Number,
+        validate(value){
+            if (value < 0) {
+                throw new Error('Age must be a positive number')
+            }
+        }
     }
 })
 
 // const me = new User({
 //     name: 'Carlos',
-//     age: 'Mike'
+//     email: 'carlos@gmail.com',
+//     password: 'pAS'
 // })
 
 // me.save().then(() => {
 //     console.log(me)
-// }).catch(() => {
+// }).catch((error) => {
 //     console.log('Error!', error)
 // })
 
 const Task = mongoose.model('Task', {
     description: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
     },
     completed: {
-        type: Boolean
+        type: Boolean,
+        default: false
     }
 })
 
 const note = new Task({
-    description: 'fazer um software',
-    completed: false
+    completed: true
 })
 
 note.save().then(() => {
     console.log(note)
-}).catch(() => {
+}).catch((error) => {
     console.log('Error!', error)
 })
