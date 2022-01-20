35e589c46305b69cf6f4825844cbc7fbe5712fa3
cahzerohenrique@gmail.com <CarlosDevBr>
2022-01-20 13:13:38 -0300

    adicionado update para user com async

--- playground/promise-chaining-2.js
+++ playground/promise-chaining-2.js
@@ -1,11 +1,23 @@
 require('../src/db/mongoose')
 const Task = require('../src/models/task')
 
-Task.findByIdAndDelete('61e95c617b87392b30d8b53a').then((task) => {
-    console.log(task)
-    return Task.countDocuments({completed: false})
-}).then((result) => {
-    console.log(result)
+// Task.findByIdAndDelete('61e95c617b87392b30d8b53a').then((task) => {
+//     console.log(task)
+//     return Task.countDocuments({completed: false})
+// }).then((result) => {
+//     console.log(result)
+// }).catch((e) => {
+//     console.log(e)
+// })
+
+const deleteIdAndCount = async (id, completed) => {
+    const task = await Task.findByIdAndDelete( id )
+    const count = await Task.countDocuments({ completed })
+    return count
+} 
+
+deleteIdAndCount('61e973c82f51f7120439efa2', true ).then((count) => {
+    console.log(count)
 }).catch((e) => {
     console.log(e)
 })
