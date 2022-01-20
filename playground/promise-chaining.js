6e139db40b3e9aa9c51b330e325ec5c6583209d0
cahzerohenrique@gmail.com <CarlosDevBr>
2022-01-20 10:06:41 -0300

    promises aninhados

--- playground/promise-chaining.js
+++ playground/promise-chaining.js
@@ -1,4 +1,11 @@
 require ('../src/db/mongoose')
 const User = require('../src/models/user')
 
-User.findByIdAndUpdate
+User.findByIdAndUpdate('61e95a76d09a2033d43092c2', {age: 1}).then((user) => {
+    console.log(user)
+    return User.countDocuments({age: 1})
+}).then((result) => {
+    console.log(result)
+}).catch((e) => {
+    console.log(e)
+});
