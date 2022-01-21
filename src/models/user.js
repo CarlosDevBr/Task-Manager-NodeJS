fe84c2f059b50b57a6e093dbd959422248ca008b
cahzerohenrique@gmail.com <CarlosDevBr>
2022-01-21 16:27:12 -0300

    adicionado token ao user

--- src/models/user.js
+++ src/models/user.js
@@ -39,12 +39,23 @@ const userSchema = new mongoose.Schema({
                 throw new Error('Age must be a positive number')
             }
         }
-    }
+    },
+    tokens: [{
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 })
 
 userSchema.methods.generateAuthToken = async function(){
     const user = this
     const token = jwt.sign({ _id: user._id.toString() }, 'thisismynewcourse')    
+    
+    user.tokens = user.tokens.concat({ token })
+    await user.save()
+
+    return token
 }
 
 userSchema.statics.findByCredentials = async (email, password) => {
