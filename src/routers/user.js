db406cc91e1a77fcd95256dc7518c6a77d9b637e
cahzerohenrique@gmail.com <CarlosDevBr>
2022-01-26 13:53:27 -0300

    adicionado rota para adicionar e remover imagem do perfil do usuario

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,50 +1,50 @@
 const express = require('express')
+const multer = require('multer')
 const User = require('../models/user')
 const auth = require('../middleware/auth')
 const router = new express.Router()
-const multer = require('multer')
 
 router.post('/users', async (req, res) => {
     const user = new User(req.body)
 
     try {
-        const token = await user.generateAuthToken()
         await user.save()
-        res.status(201).send({user, token})
-    } catch(e) {
+        const token = await user.generateAuthToken()
+        res.status(201).send({ user, token })
+    } catch (e) {
         res.status(400).send(e)
     }
 })
 
-router.post('/users/login', async(req, res) => {
-    try{
+router.post('/users/login', async (req, res) => {
+    try {
         const user = await User.findByCredentials(req.body.email, req.body.password)
         const token = await user.generateAuthToken()
-        res.send({user , token})
-    } catch(e) {
-        res.status(400).send
+        res.send({ user, token })
+    } catch (e) {
+        res.status(400).send()
     }
 })
 
 router.post('/users/logout', auth, async (req, res) => {
-    try{
+    try {
         req.user.tokens = req.user.tokens.filter((token) => {
             return token.token !== req.token
         })
         await req.user.save()
 
         res.send()
-    } catch(e) {
+    } catch (e) {
         res.status(500).send()
     }
 })
 
 router.post('/users/logoutAll', auth, async (req, res) => {
-    try{
+    try {
         req.user.tokens = []
         await req.user.save()
         res.send()
-    } catch(e) {
+    } catch (e) {
         res.status(500).send()
     }
 })
@@ -66,7 +66,7 @@ router.patch('/users/me', auth, async (req, res) => {
         updates.forEach((update) => req.user[update] = req.body[update])
         await req.user.save()
         res.send(req.user)
-    } catch(e) {
+    } catch (e) {
         res.status(400).send(e)
     }
 })
@@ -75,32 +75,36 @@ router.delete('/users/me', auth, async (req, res) => {
     try {
         await req.user.remove()
         res.send(req.user)
-    } catch(e) {
+    } catch (e) {
         res.status(500).send()
     }
 })
 
 const upload = multer({
-    dest: 'avatars',
     limits: {
         fileSize: 1000000
     },
     fileFilter(req, file, cb) {
-        if(!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
-            return cb(new Error('Please uplaod a image jpg, jpeg or png'))
+        if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+            return cb(new Error('Please upload an image'))
         }
 
         cb(undefined, true)
-        // cb(new Error('File must be a PDF'))
-        // cb(undefined, true)
-        // cb(undefined, false)   
     }
 })
 
-router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
+router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
+    req.user.avatar = req.file.buffer
+    await req.user.save()
     res.send()
 }, (error, req, res, next) => {
     res.status(400).send({ error: error.message })
+})
+
+router.delete('/users/me/avatar', auth, async (req, res) => {
+    req.user.avatar = undefined
+    await req.user.save()
+    res.send()
 })
 
 module.exports = router
