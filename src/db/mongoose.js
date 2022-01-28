0e176e6afcb43ec9ca1d32122849eba1c2711e25
cahzerohenrique@gmail.com <CarlosDevBr>
2022-01-28 17:42:58 -0300

    testando aplicação express

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useUnifiedTopology: true,
     useFindAndModify: false
 }).then(() => {
     console.log('MongoDB successfully connected. Database URL:', process.env.MONGODB_URL);
